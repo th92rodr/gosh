54d528a0e08528cac5faa42fb4227de4c3c8ab51
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-12-29 16:14:37 -0300

    gosh: rewrite get input functions

--- gosh/input.go
+++ gosh/input.go
@@ -3,274 +3,135 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"time"
 )
 
-func (t *terminal) Prompt() (string, error) {
+func (t *terminal) prompt() (string, error) {
 	fmt.Fprint(os.Stdout, promptText)
 
 	t.getColumns()
 
 	t.line = make([]rune, 0)
 	t.position = 0
+
+	t.pendingEsc = make([]rune, 0)
+	t.escIsOn = false
+
 	t.historyPosition = len(t.history)
 	t.historyEnd = ""
 
 	t.startPrompt()
 
+	var timeout <-chan time.Time
+
 mainLoop:
 	for {
-		next, err := t.readNext()
-		if err != nil {
-			return "", err
-		}
-
-		switch value := next.(type) {
-		case rune:
-			switch value {
-			case cr, lf:
-				fmt.Fprintln(os.Stdout)
+		select {
+		case input, ok := <-t.nextInput:
+			if !ok || input.err != nil || input.char == ENTER {
 				break mainLoop
-			case ctrlA: // Start of line
-				t.ctrlA()
-			case ctrlE: // End of line
-				t.ctrlE()
-			case ctrlB: // left
-				t.ctrlB()
-			case ctrlF: // right
-				t.ctrlF()
-			case ctrlD: // del
-				if err := t.ctrlD(); err != nil {
-					return "", err
-				}
-			case ctrlK: // delete remainder of line
-				t.ctrlK()
-			case ctrlP: // up
-				t.ctrlP()
-			case ctrlN: // down
-				t.ctrlN()
-			case ctrlT: // transpose prev rune with rune under cursor
-				t.ctrlT()
-			case ctrlL: // clear screen
-				t.ctrlL()
-			case ctrlH, bs: // Backspace
-				t.ctrlH()
-			case ctrlU: // Erase entire line
-				t.ctrlU()
-			// Catch unhandled control codes (anything <= 31)
-			case 0, 3, 7, 9, 15:
-				fallthrough
-			case 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31:
-				fmt.Print(beep)
-			default:
+			}
+
+			char := input.char
+
+			if char == ESC || t.escIsOn {
+				t.escIsOn = true
+				t.pendingEsc = append(t.pendingEsc, char)
+
+				// Wait for the rest of the escape sequence during 50 ms
+				// If nothing else arrives, it was an actual ESC key
+				timeout = time.After(50 * time.Millisecond)
+			} else {
 				if t.position == len(t.line) && len(promptText)+len(t.line) < t.columns {
-					t.line = append(t.line, value)
-					fmt.Printf("%c", value)
+					t.line = append(t.line, char)
+					fmt.Printf("%s", string(char))
 					t.position++
 				} else {
-					t.line = append(t.line[:t.position], append([]rune{value}, t.line[t.position:]...)...)
+					t.line = append(t.line[:t.position], append([]rune{char}, t.line[t.position:]...)...)
 					t.position++
 					t.refresh(string(t.line), t.position)
 				}
 			}
 
-		case action:
-			switch value {
-			case del:
-				t.delete()
-			case left:
-				t.left()
-			case right:
-				t.right()
-			case up:
-				t.up()
-			case down:
-				t.down()
-			}
-
-			t.refresh(string(t.line), t.position)
+		case <-timeout:
+			break mainLoop
 		}
 	}
 
-	return string(t.line), nil
+	if len(t.pendingEsc) > 0 {
+		if t.executeEscapeKey() {
+			goto mainLoop
+		}
+	}
+
+	if len(t.line) > 0 {
+		return string(t.line), nil
+	}
+
+	return "", nil
 }
 
 func (t *terminal) startPrompt() {
-	next := make(chan nexter)
+	next := make(chan input)
+
+	// keep reading inputs until an end condition is reached
 	go func() {
 		for {
-			var n nexter
-			n.r, _, n.err = t.reader.ReadRune()
-			next <- n
+			var i input
+			i.char, _, i.err = t.reader.ReadRune()
+			next <- i
 
-			// Shut down nexter loop when an end condition has been reached
-			if n.err != nil || n.r == '\n' || n.r == '\r' || n.r == ctrlC || n.r == ctrlD {
+			// Stop next input loop when an end condition has been reached
+			if i.err != nil || i.char == '\n' || i.char == '\r' || i.char == ENTER {
 				close(next)
 				return
 			}
 		}
 	}()
-	t.next = next
+
+	t.nextInput = next
 }
 
-func (t *terminal) readNext() (interface{}, error) {
-	if len(t.pending) > 0 {
-		rv := t.pending[0]
-		t.pending = t.pending[1:]
-		return rv, nil
-	}
-
-	var r rune
-
-	select {
-	case thing := <-t.next:
-		if thing.err != nil {
-			return nil, thing.err
-		}
-		r = thing.r
-	case <-t.winch:
-		t.getColumns()
-		return winch, nil
-	}
-
-	if r != esc {
-		return r, nil
-	}
-	t.pending = append(t.pending, r)
-
-	timeout := time.After(50 * time.Millisecond)
-	flag, err := t.nextPending(timeout)
-	if err != nil {
-		if err == timedOut {
-			return flag, nil
-		}
-		return unknown, err
-	}
-
-	switch flag {
-	case '[':
-		code, err := t.nextPending(timeout)
-		if err != nil {
-			if err == timedOut {
-				return code, nil
+func (t *terminal) executeEscapeKey() bool {
+	for index, key := range keys {
+		if ok := isEqual(t.pendingEsc, key); ok {
+			switch keysArrayIndexMapsToKeyName[index] {
+			case "home", "ctrlA":
+				t.home()
+			case "end", "ctrlE":
+				t.end()
+			case "right", "ctrlF":
+				t.right()
+			case "left", "ctrlB":
+				t.left()
+			case "delete":
+				t.delete()
 			}
-			return unknown, err
-		}
-		switch code {
-		case 'A':
-			t.pending = t.pending[:0] // escape code complete
-			return up, nil
-		case 'B':
-			t.pending = t.pending[:0] // escape code complete
-			return down, nil
-		case 'C':
-			t.pending = t.pending[:0] // escape code complete
-			return right, nil
-		case 'D':
-			t.pending = t.pending[:0] // escape code complete
-			return left, nil
-		case 'Z':
-			t.pending = t.pending[:0] // escape code complete
-			return shiftTab, nil
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			num := []rune{code}
-			for {
-				code, err := t.nextPending(timeout)
-				if err != nil {
-					if err == timedOut {
-						return code, nil
-					}
-					return nil, err
-				}
-				if code < '0' || code > '9' {
-					if code != '~' {
-						// escape code went off the rails
-						rv := t.pending[0]
-						t.pending = t.pending[1:]
-						return rv, nil
-					}
-					break
-				}
-				num = append(num, code)
-			}
-			t.pending = t.pending[:0] // escape code complete
-			x, _ := strconv.ParseInt(string(num), 10, 32)
-			switch x {
-			case 2:
-				return insert, nil
-			case 3:
-				return del, nil
-			case 5:
-				return pageUp, nil
-			case 6:
-				return pageDown, nil
-			case 15:
-				return f5, nil
-			case 17:
-				return f6, nil
-			case 18:
-				return f7, nil
-			case 19:
-				return f8, nil
-			case 20:
-				return f9, nil
-			case 21:
-				return f10, nil
-			case 23:
-				return f11, nil
-			case 24:
-				return f12, nil
-			default:
-				return unknown, nil
-			}
-		}
 
-	case 'O':
-		code, err := t.nextPending(timeout)
-		if err != nil {
-			if err == timedOut {
-				return code, nil
+			if t.needRefresh {
+				t.refresh(string(t.line), t.position)
 			}
-			return nil, err
+
+			t.pendingEsc = t.pendingEsc[:0]
+			t.escIsOn = false
+
+			return true
 		}
-		t.pending = t.pending[:0] // escape code complete
-		switch code {
-		case 'H':
-			return home, nil
-		case 'F':
-			return end, nil
-		case 'P':
-			return f1, nil
-		case 'Q':
-			return f2, nil
-		case 'R':
-			return f3, nil
-		case 'S':
-			return f4, nil
-		default:
-			return unknown, nil
-		}
-	default:
-		rv := t.pending[0]
-		t.pending = t.pending[1:]
-		return rv, nil
 	}
 
-	return nil, nil
+	return false
 }
 
-func (t *terminal) nextPending(timeout <-chan time.Time) (rune, error) {
-	select {
-	case thing := <-t.next:
-		if thing.err != nil {
-			return 0, thing.err
-		}
-		t.pending = append(t.pending, thing.r)
-		return thing.r, nil
-	case <-timeout:
-		rv := t.pending[0]
-		t.pending = t.pending[1:]
-		return rv, timedOut
+func isEqual(a, b []rune) bool {
+	if len(a) != len(b) {
+		return false
 	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
